ea8eda7bdafb0b3219f851927fcacb980dbf44c5
FTS427 <FTS427@outlook.com>
2024-10-18 17:13:51 +0800

    F | fix some places

--- src/endstone_real_weather/real_weather.py
+++ src/endstone_real_weather/real_weather.py
@@ -9,23 +9,17 @@ class RealWeather(Plugin):
         super().__init__()
         self.update_period:float = 0
         self.city:str = ""
-        
+
         self.url:str = 'http://t.weather.sojson.com/api/weather/city/'
-        self.update_time:int = int(self.update_period*72000)
+        self.update_time:int = self.update_period*72000
 
     def on_enable(self) -> None:
         self.save_default_config()
         self.load_config()
-        
-        if not os.path.exists("plugins/real_weather/city.json"): # Get city.json from repo
-            city_json=requests.get("https://raw.githubusercontent.com/https://github.com/ZH-Server/endstone_real_weather/main/assets/city.json")
-            if city_json.status_code == 200:
-                with open("plugins/real_weather/city.json", "wb") as file:
-                    file.write(city_json.content)
-                self.logger.info("Dependent file is ready!")
-            else:
-                self.logger.error("Missing dependent file! Go to https://github.com/ZH-Server/endstone_real_weather/ read README.md first!")
-        
+
+        if not os.path.exists("plugins/real_weather/city.json"):
+            self.logger.error("Missing dependent file! Go to https://github.com/ZH-Server/endstone_real_weather/ read README.md first!")
+
         self.server.scheduler.run_task(self, self.update_weather, delay=0, period = self.update_time)
 
     commands = {
@@ -47,16 +41,19 @@ class RealWeather(Plugin):
         if command.name == "realweather":
             if str(args[0]) == "sync":
                 if len(str(args[1])) > 0:
-                    self.server.dispatch_command(self.server.command_sender, f"weather {self.sync_weather(str(args[1]))}")
-                    sender.send_message(f"{str(args[1])}'s weather: {self.sync_weather(str(args[1]))}")
-                    sender.send_message(f"Has synchronized {args[1]} weather")
+                    if not self.sync_weather(str(args[1])) != "ERROR":
+                        sender.send_message(f"{str(args[1])}'s weather: {self.sync_weather(str(args[1]))}")
+                        self.server.dispatch_command(self.server.command_sender, f"weather {self.sync_weather(str(args[1]))}")
+                        sender.send_message(f"Has synchronized {args[1]} weather")
+                    else:
+                        sender.send_error_message("Wrong city's name!")
                 else:
                     self.server.dispatch_command(self.server.command_sender, f"weather {self.sync_weather(str(self.city))}")
                     sender.send_message(f"{str(self.city)}'s weather: {self.sync_weather(str(self.city))}")
                     sender.send_message("Has used default config to sync weather")
             if str(args[0]) == "info":
                 if len(str(args[1])) > 0:
-                    if self.sync_weather(str(args[1])) != "None":
+                    if self.sync_weather(str(args[1])) != "ERROR":
                         sender.send_message(f"{str(args[1])}'s weather: {self.sync_weather(str(args[1]))}")
                     else:
                         sender.send_error_message("Wrong city's name!")
@@ -68,7 +65,7 @@ class RealWeather(Plugin):
         f = open("plugins/real_weather/city.json", 'rb')
         cities = json.load(f)
         city = cities.get(city)
-        
+
         if city != None:
             response = requests.get(self.url + city)
 
@@ -89,7 +86,7 @@ class RealWeather(Plugin):
         else:
             self.logger.error("Wrong city!")
             return "ERROR"
-    
+
     def change_weather(self, weather:str, time: int) -> None:
         if weather != "ERROR":
             self.server.dispatch_command(self.server.command_sender, "gamerule doWeatherCycle false")
@@ -97,10 +94,10 @@ class RealWeather(Plugin):
         else:
             self.server.dispatch_command(self.server.command_sender, "gamerule doWeatherCycle true")
             self.logger.error("Wrong weather")
-    
+
     def update_weather(self) -> None:
         self.change_weather(self.sync_weather(str(self.city)), self.update_time)
-    
+
     def load_config(self) -> None:
         self.city=self.config["city"]
         self.update_period=self.config["update_period"]
